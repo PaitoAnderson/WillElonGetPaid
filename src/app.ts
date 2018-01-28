bcedcc4d54667b2643b83790fc89e0638dee89cf
Paito Anderson <pj.paito@gmail.com>
2018-01-28 16:28:28 -0500

    Record vote

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 require('./style.css');
 
-/*import * as firebase from 'firebase'
+import * as firebase from 'firebase';
 // Required for side-effects
 require("firebase/firestore");
 
@@ -13,9 +13,10 @@ firebase.initializeApp({
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-db.collection("votes").get().then((querySnapshot) => {
+// Get Vote Counts
+/*db.collection("votes").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
+        console.log(doc.id, doc.data());
     });
 });*/
 
@@ -26,18 +27,31 @@ voteNoElement.onclick = voteNo;
 voteYesElement.onclick = voteYes;
 
 function voteNo() {
-    removeByClassName('segmented-control-input');
-
-    console.log('Votes No!');
+    vote(false);
 }
 
 function voteYes() {
-    removeByClassName('segmented-control-input');
-
-    console.log('Voted Yes!');
+   vote(true);
 }
 
-function removeByClassName(className) {
+function vote(value: boolean) {
+    removeByClassName('segmented-control-input');
+
+    // Set Vote
+    db.collection("votes").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        useragent: navigator.userAgent,
+        value,
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+
+function removeByClassName(className: string) {
     var x = document.getElementsByClassName(className);
     var i;
     for (i = x.length - 1; i >= 0; i--){
